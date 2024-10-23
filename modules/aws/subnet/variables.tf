ea9cb6d62c698a3f9b9e59f83c3e02d13c7a5c3d
Rafael Cavalcante <rvmascavalcante@gmail.com>
2024-10-22 23:01:22 -0300

    feat: add subnet module

---
+++ modules/aws/subnet/variables.tf
@@ -0,0 +1,20 @@
+variable "vpc_id" {
+  description = "VPC ID to create the subnet in"
+  type        = string
+}
+
+variable "cidr_block" {
+  description = "CIDR block for the subnet"
+  type        = string
+}
+
+variable "map_public_ip_on_launch" {
+  description = "Whether to map public IP on launch"
+  type        = bool
+  default     = true
+}
+
+variable "name" {
+  description = "Name tag for the subnet"
+  type        = string
+}
