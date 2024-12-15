73d117a7e0b2d075d8d9eb9dd721831cd937117a
Rafael Cavalcante <rvmascavalcante@gmail.com>
2024-12-15 17:27:50 -0300

    chore: remove required providers

--- providers.tf
+++ providers.tf
@@ -1,13 +1,4 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.52.0"
-    }
-  }
-
-  required_version = ">= 1.1.0"
-
   cloud {
     organization = "FIAP-Lanchonete-G41"
 
